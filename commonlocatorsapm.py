3dda72e3e32f1e954e2e85afdec32314529dbf59
Raja Sekhar Sandra <69429414+Sandra-Rajasekhar@users.noreply.github.com>
2022-05-12 15:28:42 +0530

    Add files via upload

---
+++ commonlocatorsapm.py
@@ -0,0 +1,17 @@
+class CommonLocatorsAPM():
+    """ CommonLocators """
+    USERNAME_ID = 'id=username'
+    PASSWORD_ID = 'id=phenomPassword3'
+    # USER_NAME = 'id=user-name'
+    USER_NAME = "xpath=//div[@class='user-name' and text()='Raja Sekhar Sandra']"
+    CONTINUE_BUTTON = 'id=kc-login'
+    DASHBOARD_SELECT_BOX = "xpath=//span[text()='Default Alert Dashboard']"
+    LIVE_SWITCH = "xpath=//span[text()='LIVE']"
+    LIVE_SWITCH_ON = "xpath=//button[@class='ant-switch ant-switch-checked']"
+    LIVE_SWITCH_OFF = "xpath=//button[@class='ant-switch']"
+    CALENDAR_WIDGET = "xpath=//div[@class='ant-picker-range-separator']"
+    GO_TO_APPLY_BUTTON = "xpath=//a[text()='Go to Apply']"
+    GO_TO_HSI_BUTTON = "xpath=//a[text()='Go to HSI']"
+    GO_TO_JOB_SYNC_BUTTON = "xpath=//a[text()='Go to Job Sync']"
+    GO_TO_GRAFANA_BUTTON = "xpath=//a[text()='Go to Grafana']"
+    GO_TO_CHATBOT_BUTTON = "xpath=//a[text()='Go to Chatbot']"
